e5b9aa182a4a666743ebc8c39ad623016c332488
Mikko Kujala <m.kujala@live.com>
2025-07-17 00:38:55 +0300

    add types to export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 
 const binding = require('bindings');
 import { type VL53L5CX } from "./types"
+import { ScanData, ScanZone, ScanTarget } from "./types"
 export const bindings: VL53L5CX = binding('vl53l5cx_native');
+export { ScanData, ScanZone, ScanTarget }
